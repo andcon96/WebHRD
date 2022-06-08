ca6ca503cb620641e7c1e99cbe0498dabbb0e0ab
andcon96 <78584931+andcon96@users.noreply.github.com>
2022-06-08 14:28:03 +0700

    tst

--- app/Services/WSAServices.php
+++ app/Services/WSAServices.php
@@ -185,7 +185,7 @@ class WSAServices
         $dataloop    = $xmlResp->xpath('//ns1:tempRow');
         
         $qdocResult = (string) $xmlResp->xpath('//ns1:outOK')[0];
-
+        dd($qdocResponse,$dataloop);
         if($qdocResult == 'true'){
             DB::beginTransaction();
             try{
