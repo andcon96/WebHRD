785035178ece1c12b29f3d8284f51b105874c5fc
andcon96 <78584931+andcon96@users.noreply.github.com>
2022-06-09 14:31:23 +0700

    tst

--- app/Services/QxtendServices.php
+++ app/Services/QxtendServices.php
@@ -500,6 +500,8 @@ class QxtendServices
     if (is_bool($qdocResponse)) {
       return false;
     }
+    dd($qdocResponse);
+
     $xmlResp = simplexml_load_string($qdocResponse);
 
     $xmlResp->registerXPathNamespace('ns1', 'urn:schemas-qad-com:xml-services');
